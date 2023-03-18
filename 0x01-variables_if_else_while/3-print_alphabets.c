757906e34abc6a42ac04f92e3c0e1857b29e5af3
Seydouh <sayddipsy@gmail.com>
2023-03-18 08:13:47 -0700

    changes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,10 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++) putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)  putchar(ch);
-	putchar('n\');
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
 	return (0);
 }

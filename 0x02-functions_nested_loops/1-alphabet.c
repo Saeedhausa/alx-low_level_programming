11ef96bf1fc9f5aa2a3e29a0f2fdb73c7d8afa1d
Seydouh <sayddipsy@gmail.com>
2023-03-21 08:59:51 -0700

    changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,12 @@
 /**
  * print _alphabet - print all alphabets in lowercase
  */
-void print _alphabet(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 }

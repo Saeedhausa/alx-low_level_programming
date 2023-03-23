562df504d84a414bea26e7176fb9ec7ca3f4a812
Seydouh <sayddipsy@gmail.com>
2023-03-23 03:50:52 -0700

    changes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,15 @@
 #include "main.h"
+
 /**
-<<<<<<< HEAD
-* main - Test function for positive or negative
-* Return: 0
-*/
-int main(void)
-{
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-=======
- * main - Tests function for positive or negative
+ * main - Test function for positive or negative
  * Return: 0
  */
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
+        i = 0;
 
-	positive_or_negative(i);
-
-	return (0);
->>>>>>> 42c698453d29401d00de948451315d1d182651b7
+        positive_or_negative(i);
+        return (0);
 }
-
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,12 @@
 #ifndef MAIN_H
-#define MAIN_H
-<<<<<<< HEAD
-=======
 
->>>>>>> 42c698453d29401d00de948451315d1d182651b7
+#define MAIN_H
+
 #include <stdio.h>
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-<<<<<<< HEAD
+
 #endif
-
-=======
-
-#endif /* MAIN_H */
->>>>>>> 42c698453d29401d00de948451315d1d182651b7

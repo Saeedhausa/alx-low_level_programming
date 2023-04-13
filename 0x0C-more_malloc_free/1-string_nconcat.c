4b8fe7ccd8742c9c84929a9673d33f1b46a0502b
Seydouh <sayddipsy@gmail.com>
2023-04-13 07:00:00 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		y = n;
 		s = malloc(sizeof(char) * (x + y + 1));
 	if (s == NULL)
-	return (NULL);
+		return (NULL);
 	for (z = 0; z < x; z++)
 		s[z] = s1[z];
 	for (z = 0; z < y; z++)
